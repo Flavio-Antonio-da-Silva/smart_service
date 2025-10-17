aa1c03235f26d3f961a7d0bb4a6244184454dc11
Flávio Antônio <flavusantonio@gmail.com>
2025-10-17 17:53:19 -0300

    Update vite.config.js

--- s_service/vite.config.js
+++ s_service/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),
-               react()
-                           ],
-})
+  plugins: [tailwindcss(), react()],
+  base: "/", // garante que os assets sejam buscados na raiz do domínio
+  build: {
+    outDir: "dist" // garante que o output vá exatamente para dist/
+  }
+});
